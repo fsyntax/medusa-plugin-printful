8e34f0fe2d61832889865cdcacdaa824a39f0120
fsyntax <fsyntax@protonmail.com>
2023-06-15 10:01:28 +0200

    [refactor]: configured the products worker to use the already provided REDIS_URL

--- src/services/productsQueue.ts
+++ src/services/productsQueue.ts
@@ -9,57 +9,75 @@ class ProductsQueueService extends TransactionBaseService {
     private productWorker_: Worker;
     private productService: ProductService;
     private logger_: Logger;
-  constructor(container, options) {
-    super(container);
-    this.printfulService_ = container.printfulService;
-    this.eventBusService_ = container.eventBusService;
-    this.productService = container.productService;
-    this.manager_ = container.manager;
-    this.logger_ = container.logger;
+    private redisURL_: string;
 
-    this.queue_ = new Queue('printful-products', { connection: { host: options.redisHost, port: options.redisPort } });
-    this.queue_.obliterate().then(() => {
-        console.log(`${blueBright("[medusa-plugin-printful]:")} Queue obliterated!`)
-    })
-    this.productWorker_ = new Worker("printful-products",
-        async (job: Job) => {
-            try {
-                this.logger_.info(`Processing job #${job.id}`);
-                console.info(`${blueBright("[medusa-plugin-printful]:")} Processing job #${job.id} - [${job.data.id}] ${blueBright(job.data.name)}`)
-                const { id } = job.data;
+    constructor(container, options) {
+        super(container);
+        this.printfulService_ = container.printfulService;
+        this.eventBusService_ = container.eventBusService;
+        this.productService = container.productService;
+        this.manager_ = container.manager;
+        this.logger_ = container.logger;
+        this.redisURL_ = options.redisURL;
+        const redisConfig = this.parseRedisURL(this.redisURL_);
 
-                const { sync_product, sync_variants } = await this.printfulService_.getSyncProduct(id);
-                const products = await this.productService.list({external_id: id});
-
-                if (products.length === 1) {
-                        console.log(`${blue("[medusa-plugin-printful]:")} Updating product with external ID ${id}`)
-                    const existingProduct = await this.productService.retrieve(products[0].id, {    relations: ["variants", "options"]});
-                        console.log(`${blue("[medusa-plugin-printful]:")} Retrieved product with ID ${existingProduct.id} from Medusa, trying to update it`)
-                   return await this.printfulService_.updateMedusaProduct({sync_product, sync_variants, medusa_product: existingProduct}, "fromPrintful", null)
-                } else if(products.length === 0) {
-                  return await this.printfulService_.createMedusaProduct({sync_product, sync_variants})
-                }
-            } catch (e) {
-                console.error(`Job failed with ID ${job.id}: ${e.result}`);
-                if (e.code === 429) {
-                    const duration = parseInt(e.error.message.match(/try again after (\d+)/)[1]);
-                    console.log(`${yellow("[medusa-plugin-printful]:")} Rate limit reached. Pausing queue for ${yellow(duration)} seconds.`);
-                    await this.queue_.pause();
-                    setTimeout(() => this.queue_.resume(), duration * 1000);
-                } else {
-                    throw e.result
-                }
-            }
-        },
-        {
-            concurrency: 1,
-            limiter: { max: 300, duration: 1000 },
-
+        this.queue_ = new Queue('printful-products', {
+            connection: {
+                host: redisConfig.host,
+                port: redisConfig.port,
+                password: redisConfig.password,
+                db: redisConfig.db,
+            },
         });
-  }
+        console.log(this.queue_)
+        this.queue_.obliterate().then(() => {
+            console.log(`${blueBright("[medusa-plugin-printful]:")} Queue obliterated!`)
+        })
+
+
+        this.productWorker_ = new Worker("printful-products",
+            async (job: Job) => {
+                try {
+                    this.logger_.info(`Processing job #${job.id}`);
+                    console.info(`${blueBright("[medusa-plugin-printful]:")} Processing job #${job.id} - [${job.data.id}] ${blueBright(job.data.name)}`)
+                    const {id} = job.data;
+
+                    const {sync_product, sync_variants} = await this.printfulService_.getSyncProduct(id);
+                    const products = await this.productService.list({external_id: id});
+
+                    if (products.length === 1) {
+                        console.log(`${blue("[medusa-plugin-printful]:")} Updating product with external ID ${id}`)
+                        const existingProduct = await this.productService.retrieve(products[0].id, {relations: ["variants", "options"]});
+                        console.log(`${blue("[medusa-plugin-printful]:")} Retrieved product with ID ${existingProduct.id} from Medusa, trying to update it`)
+                        return await this.printfulService_.updateMedusaProduct({
+                            sync_product,
+                            sync_variants,
+                            medusa_product: existingProduct
+                        }, "fromPrintful", null)
+                    } else if (products.length === 0) {
+                        return await this.printfulService_.createMedusaProduct({sync_product, sync_variants})
+                    }
+                } catch (e) {
+                    console.error(`Job failed with ID ${job.id}: ${e.result}`);
+                    if (e.code === 429) {
+                        const duration = parseInt(e.error.message.match(/try again after (\d+)/)[1]);
+                        console.log(`${yellow("[medusa-plugin-printful]:")} Rate limit reached. Pausing queue for ${yellow(duration)} seconds.`);
+                        await this.queue_.pause();
+                        setTimeout(() => this.queue_.resume(), duration * 1000);
+                    } else {
+                        throw e.result
+                    }
+                }
+            },
+            {
+                concurrency: 1,
+                limiter: {max: 300, duration: 1000},
+
+            });
+    }
 
     async addJob(productPayload) {
-       console.log(`${blue("[medusa-plugin-printful]:")} Adding job to queue!`)
+        console.log(`${blue("[medusa-plugin-printful]:")} Adding job to queue!`)
         await this.queue_.add("printful-product-webhook-event", productPayload);
     }
 
@@ -68,6 +86,28 @@ class ProductsQueueService extends TransactionBaseService {
         await this.queue_.addBulk(jobsData);
         console.log(`[medusa-plugin-printful]: Successfully added ${jobsData.length} jobs to the queue.`);
     }
+
+    parseRedisURL(redisURL) {
+        const url = new URL(redisURL);
+
+        const redisConfig = {
+            host: url.hostname,
+            port: parseInt(url.port),
+            db: parseInt(url.pathname.substr(1)),
+        };
+
+        if (url.username || url.password) {
+            redisConfig.password = url.password;
+        }
+
+        if (isNaN(redisConfig.db)) {
+            redisConfig.db = 0; // Set the default database to 0
+        }
+
+        return redisConfig;
+    }
+
+
 }
 
 export default ProductsQueueService;
